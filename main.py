3f86b875004fd0fa8d14eb75540586b90a42d275
Di Zhao <zd277607529@gmail.com>
2023-09-04 15:09:52 +1200

    update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import copy
 import json
+import time
 import torch
+import torch.backends.cudnn as cudnn
 import argparse
 from data.dataset import DomainDataset
 
@@ -10,6 +14,7 @@ parser.add_argument("--target", default="clipart")
 parser.add_argument("--data_root", default="/dataset/", type=str)
 parser.add_argument("--runs", default=10, type=int)
 parser.add_argument("--log_dir", default="./logs", type=str)
+parser.add_argument("--ckpt_dir", default="./checkpoints", type=str)
 parser.add_argument("--config_file", default=None)
 
 args = parser.parse_args()
@@ -23,8 +28,42 @@ with open(config_file) as json_file:
 print("Config File: {}".format(config_file))
 # print(configs)
 
-target = args.target
 multi_domain = True
+input_dim = 2048
+configs['freeze_bn'] = False
+semantic_w = 1.0
+
 DOMAINS = DNET_DOMAINS
+target = args.target
+sources = copy.deepcopy(DNET_DOMAINS)
+sources.remove(target)
 dataset = DomainDataset
-print(dataset)
+
+configs['input_dim'] = input_dim
+configs['semantic_w'] = semantic_w
+configs['multi_domain'] = multi_domain
+
+log_dir = args.log_dir
+checkpoint_dir = args.ckpt_dir
+cudnn.benchmark = True
+exp_name = "model.pkl"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+log_file = os.path.join(log_dir, (time.strftime("%Y-%m-%d-%H-%M-%S") + ".log"))
+checkpoint_file = os.path.join(checkpoint_dir, time.strftime("%Y-%m-%d-%H-%M-%S") + "-runN.pth")
+
+logger = {"results": [], "config": configs, "target": target, "checkpoints": [],
+          "sem_loss": [[] for _ in range(args.runs)], "mimg_loss": [[] for _ in range(args.runs)],
+          "mfeat_loss": [[] for _ in range(args.runs)]}
+
+results = []
+results_top = []
+val_datasets = None
+
+for r in range(args.runs):
+    print("Target: {}         Run: {} / {}".format(target, str(r+1), args.runs))
+    train_dataset = dataset(args.data_root, sources, train=True)
+    # test_dataset = dataset(args.data_root, target, train=False)
